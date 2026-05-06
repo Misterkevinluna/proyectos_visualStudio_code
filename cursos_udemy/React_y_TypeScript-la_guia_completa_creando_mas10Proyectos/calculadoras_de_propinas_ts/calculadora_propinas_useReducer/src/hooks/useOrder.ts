a890abef19ed4c891a76aeab8870549543686941
Misterkevinluna <kevinlunasjimenez@gmail.com>
2026-05-06 13:22:53 -0500

    cambio del nombre de la carpeta del proyecto de guitarras con useReducer, tenia un espacio

---
+++ cursos_udemy/React_y_TypeScript-la_guia_completa_creando_mas10Proyectos/calculadoras_de_propinas_ts/calculadora_propinas_useReducer/src/hooks/useOrder.ts
@@ -0,0 +1,56 @@
+import { useState, useCallback } from "react";
+import type { MenuItem, OrderItem } from "../types";
+
+export default function useOrder() {
+    const [order, setOrder] = useState<OrderItem[]>([]);
+    const [tip, setTip] = useState(0);
+
+    // const addItem = (item: MenuItem) => {
+    //     const itemExist = order.find(orderItem => orderItem.id === item.id);
+    //     if (itemExist) {
+    //         const updatedOrder = order.map(orderItem => orderItem.id === item.id 
+    //         ? {...orderItem, quantity: orderItem.quantity + 1} 
+    //         : orderItem);
+    //         setOrder(updatedOrder);
+    //     }else{
+    //         setOrder([...order, {...item, quantity: 1}]);
+    //     }
+    //     console.log(order);
+    // }
+
+    const addItem = useCallback((item: MenuItem) => {
+        setOrder(prevOrder => {
+            const index = prevOrder.findIndex(orderItem => orderItem.id === item.id);
+
+            if (index !== -1) {
+                // actualizar en una sola pasada
+                const updatedOrder = [...prevOrder];
+                updatedOrder[index] = {
+                    ...updatedOrder[index],
+                    quantity: updatedOrder[index].quantity + 1
+                };
+                return updatedOrder;
+            }
+
+            return [...prevOrder, { ...item, quantity: 1 }];
+        });
+    }, []);
+
+    const removeItem = (id: MenuItem['id']) => {
+        setOrder(listOrder => listOrder.filter(order => order.id !== id));
+    }
+
+    const placeOrder = () => {
+        setOrder([]);
+        setTip(0);
+    };
+
+    return {
+        order,
+        tip,
+        setTip,
+        addItem,
+        removeItem,
+        placeOrder
+    }
+}
