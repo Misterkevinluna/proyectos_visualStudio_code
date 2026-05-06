343a918f127192dd8d06aed4f699e6afa6b8bd21
Misterkevinluna <kevinlunasjimenez@gmail.com>
2026-05-06 10:49:38 -0500

    agregando una corrección a la vista del nombre de las guitarras en el carrito de compras, antes me estaba apareciendo el codigo enel nombre

--- cursos_udemy/React_y_TypeScript-la_guia_completa_creando_mas10Proyectos/proyecto_guitarla_ts/guitarla-ts -useReducer/src/components/Header.tsx
+++ cursos_udemy/React_y_TypeScript-la_guia_completa_creando_mas10Proyectos/proyecto_guitarla_ts/guitarla-ts -useReducer/src/components/Header.tsx
@@ -64,7 +64,7 @@ export default function Header({
                               alt="imagen guitarra"
                             />
                           </td>
-                          <td>guitar.name</td>
+                          <td>{guitar.name}</td>
                           <td className="fw-bold">${guitar.price}</td>
                           <td className="flex align-items-start gap-4">
                             <button type="button" className="btn btn-dark" onClick={ () => dispatch( { type: 'decrease-quantity', payload: { id: guitar.id } } )}>
